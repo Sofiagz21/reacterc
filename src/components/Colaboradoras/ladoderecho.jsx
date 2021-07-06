3342c346ff43ea74ca04be1130582c3cf7c8245f
Diana_Londoño <dianaclondonoc@gmail.com>
2021-07-05 20:53:11 -0500

    Hice la sección de colaboradoras

---
+++ src/components/Colaboradoras/ladoderecho.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Colaboradoras from '../../utils/images/Colaboradoras/colaboradoras.png'
+
+
+const ladoderecho = () => {
+    return (
+      <div className="ladoizquierdo">
+  <div className="imagenGrande">
+  <img src={Colaboradoras} alt />
+  </div>
+  <div className="textImagenGrande">
+    <p>
+      Hola! Somos desarrolladoras Front End, para nosotras es un gusto
+      presentarnos puesto que cada una tiene habilidades diferentes pero únicas
+      en este mundo tan amplio en la programación. Todo nuestro esfuerzo y
+      aprendizaje se visualiza en este proyecto pues nos dimos la oportunidad de
+      aplicar todos nuestros conocimientos en un proyecto social como lo fue
+      visualizar a algunos campesinos sin ningún animo de lucro.
+    </p>
+  </div>
+</div>
+    )
+}
+
+export default ladoderecho
---
+++ src/components/Colaboradoras/ladoizquierdo.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import Sofia from '../../utils/images/Colaboradoras/Sofia.jpeg'
+import Facebook from '../../utils/images/Colaboradoras/facebook.png'
+import Twitter from '../../utils/images/Colaboradoras/twitter.png'
+import Instagram from '../../utils/images/Colaboradoras/instagram_.png'
+import Mafe from '../../utils/images/Colaboradoras/Mafe.jpeg'
+
+
+
+
+
+const ladoizquierdo = () => {
+    return (
+        <div className="ladoderecho">
+  <div className="colaboradorasTitle">
+    <h1>SOBRE NOSOTRAS </h1>
+  </div>
+  <div className="container">
+    <div className="card">
+      {/* container imagenes y fondo */}
+      <div className="images">
+        <div className="imgBox">
+        <img src={Sofia} alt />
+        </div>
+      </div>
+      {/* contenedor caja 1 */}
+      <div class="content">
+                   <p>Developer Front End</p> 
+                   <h3>Sofia Gonzalez Zubieta</h3>
+                   {/*contenedor redes sociales*/}
+                   <div className="social">
+          <a href="https://www.facebook.com/sofia.gonzalezzubieta"> <img src={Facebook} alt />
+            <i />
+          </a>
+          <a href="https://www.instagram.com/sofiaaa_gonzalez_/"> <img src={Twitter} alt />
+            <i />
+          </a>
+          <a href="https://mobile.twitter.com/sofiaaaa_gz"> <img src={Instagram} alt />
+            <i />
+          </a>
+        </div>
+    </div>
+    </div>
+    <div className="card">
+      {/* container imagenes y fondo */}
+      <div className="images">
+        <div className="imgBox">
+        {/*<img src={} alt />*/}
+        </div>
+      </div>
+      {/* contenedor caja 1 */}
+      <div class="content">
+                   <p>Developer Front End</p> 
+                   <h3>Diana Carolina Londoño</h3>
+                   {/*contenedor redes sociales*/}
+                   <div className="social">
+          <a href="https://www.facebook.com/profile.php?id=100010720841197"> <img src={Facebook} alt />
+            <i />
+          </a>
+          <a href="https://www.instagram.com/_dianacarolina0_/"> <img src={Twitter} alt />
+            <i />
+          </a>
+          <a href="https://mobile.twitter.com/pitonxlx"> <img src={Instagram} alt />
+            <i />
+          </a>
+        </div>
+    </div>
+    </div>
+    <div className="card">
+      {/* container imagenes y fondo */}
+      <div className="images">
+        <div className="imgBox">
+        {/*<img src={Mafe} alt />*/}
+        </div>
+      </div>
+      {/* contenedor caja 1 */}
+      <div class="content">
+                   <p>Developer Front End</p> 
+                   <h3>Laura Tatiana  <br/>Celis Ballesteros</h3>
+                   
+                   {/*contenedor redes sociales*/}
+                   <div className="social">
+          <a href="https://www.facebook.com/profile.php?id=100007725649378"> <img src={Facebook} alt />
+            <i />
+          </a>
+          <a href="https://www.instagram.com/blaauratt/"> <img src={Twitter} alt />
+            <i />
+          </a>
+          <a href="https://mobile.twitter.com/blaauratt"> <img src={Instagram} alt />
+            <i />
+          </a>
+        </div>
+    </div>
+    
+    </div>
+    <div className="card">
+      {/* container imagenes y fondo */}
+      <div className="images">
+        <div className="imgBox">
+        {/*<img src={Mafe} alt />*/}
+        </div>
+      </div>
+      {/* contenedor caja 1 */}
+      <div class="content">
+                   <p>Developer Front End</p> 
+                   <h3>Maria Fernanda   <br/>Caro Carvajal</h3>
+                   
+                   {/*contenedor redes sociales*/}
+                   <div className="social">
+          <a href="https://www.facebook.com/profile.php?id=100014097480970"> <img src={Facebook} alt />
+            <i />
+          </a>
+          <a href="https://www.instagram.com/mfd_ffx/"> <img src={Twitter} alt />
+            <i />
+          </a>
+          <a href="https://mobile.twitter.com/mbx_maria"> <img src={Instagram} alt />
+            <i />
+          </a>
+        </div>
+    </div>
+    </div>
+   
+
+  </div>
+</div>
+    )
+}
+
+export default ladoizquierdo
